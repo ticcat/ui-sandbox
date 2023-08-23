5c11e3bec73e117da6cd39c93d1bab2698265944
ticcat <serfgiorico@gmail.com>
2023-08-23 15:12:37 +0200

    Added new NavBar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import NavBar from "@/components/NavBar/NavBar";
 import styles from "./styles/page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Main Page</h1>
-      <a href="/firstRouter">Go to first router</a>
-    </main>
+    <>
+      <NavBar></NavBar>
+      <main className={styles.main}>
+        <h1>Main Page</h1>
+      </main>
+    </>
   );
 }
