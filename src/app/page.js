539823858b467010f4120acc7d26d821e484f824
ticcat <serfgiorico@gmail.com>
2023-08-23 15:20:37 +0200

    Added NavBarButton component and test button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import NavBar from "@/components/NavBar/NavBar";
 import styles from "./styles/page.module.css";
+import NavBarButton from "@/components/NavBarButton/NavBarButton";
 
 export default function Home() {
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar>
+        <NavBarButton>Test</NavBarButton>
+      </NavBar>
       <main className={styles.main}>
         <h1>Main Page</h1>
       </main>
---
+++ src/components/NavBarButton/NavBarButton.jsx
@@ -0,0 +1,7 @@
+import styles from "./NavBarButton.module.css";
+
+function NavBarButton({ children }) {
+  return <button className={styles.button}>{children}</button>;
+}
+
+export default NavBarButton;
