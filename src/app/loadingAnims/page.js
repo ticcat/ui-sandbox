4bd3be907235cddd25ad42f7a74997ce6e0b7839
ticcat <serfgiorico@gmail.com>
2023-08-24 12:50:32 +0200

    Created second project routed page

---
+++ src/app/loadingAnims/page.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "./styles/page.module.css";
+import TopNavBar from "../../components/TopNavBar/TopNavBar";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>UI Sandbox</title>
+      </Head>
+      <TopNavBar></TopNavBar>
+      <main className={styles.main}>
+        <h1>Loading animations</h1>
+      </main>
+    </>
+  );
+}
