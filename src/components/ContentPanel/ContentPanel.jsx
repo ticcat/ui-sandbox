4e3c8e200879d4f1b21cc12a36f46faca4110112
ticcat <serfgiorico@gmail.com>
2023-08-30 12:15:53 +0200

    Added content cards skeleton and styling

--- src/components/ContentPanel/ContentPanel.jsx
+++ src/components/ContentPanel/ContentPanel.jsx
@@ -1,11 +1,24 @@
 import styles from "./ContentPanel.module.css";
 
-function ContentPanel() {
+function ContentCard({ children }) {
   return (
-    <>
-      <h2 className={styles.title}>Content</h2>
-    </>
+    <div className={styles.card}>
+      <div className={styles.preview}></div>
+      <h2>{children}</h2>
+    </div>
   );
+}
+
+function ContentPanel() {
+  const testProjects = [
+    <ContentCard key={1}>Button 1</ContentCard>,
+    <ContentCard key={2}>Button 2</ContentCard>,
+    <ContentCard key={3}>Button 3</ContentCard>,
+    <ContentCard key={4}>Button 4</ContentCard>,
+    <ContentCard key={5}>Button 5</ContentCard>,
+  ];
+
+  return <div className={styles.container}>{testProjects}</div>;
 }
 
 export default ContentPanel;
