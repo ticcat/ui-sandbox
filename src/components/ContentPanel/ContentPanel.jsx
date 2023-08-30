00f3575752174376c15f90324a99e7178297dc9d
ticcat <serfgiorico@gmail.com>
2023-08-30 13:05:30 +0200

    Fixed content grid rows alignment

--- src/components/ContentPanel/ContentPanel.jsx
+++ src/components/ContentPanel/ContentPanel.jsx
@@ -4,7 +4,7 @@ function ContentCard({ children }) {
   return (
     <div className={styles.card}>
       <div className={styles.preview}></div>
-      <h2>{children}</h2>
+      <h2 className={styles.title}>{children}</h2>
     </div>
   );
 }
@@ -16,6 +16,10 @@ function ContentPanel() {
     <ContentCard key={3}>Button 3</ContentCard>,
     <ContentCard key={4}>Button 4</ContentCard>,
     <ContentCard key={5}>Button 5</ContentCard>,
+    <ContentCard key={1}>Button 1</ContentCard>,
+    <ContentCard key={2}>Button 2</ContentCard>,
+    <ContentCard key={3}>Button 3</ContentCard>,
+    <ContentCard key={4}>Button 4</ContentCard>,
   ];
 
   return <div className={styles.container}>{testProjects}</div>;
