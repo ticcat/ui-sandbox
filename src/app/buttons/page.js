0141ed015e3fe7057d7a14f4d83be826f104ab06
ticcat <serfgiorico@gmail.com>
2023-08-30 10:55:51 +0200

    Deleted metadata from pages

--- src/app/buttons/page.js
+++ src/app/buttons/page.js
@@ -4,9 +4,6 @@ import styles from "./styles/page.module.css";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Buttons</title>
-      </Head>
       <main className={styles.main}>
         <h1>Buttons</h1>
       </main>
--- src/app/loadingAnims/page.js
+++ src/app/loadingAnims/page.js
@@ -4,9 +4,6 @@ import styles from "./styles/page.module.css";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>UI Sandbox</title>
-      </Head>
       <main className={styles.main}>
         <h1>Loading animations</h1>
       </main>
