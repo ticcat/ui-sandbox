106db666ded96e638ddcd16a545eaf44230d76b9
ticcat <serfgiorico@gmail.com>
2023-09-01 13:54:55 +0200

    Added valorant button study case

--- src/app/buttons/cases/ValorantButton/ValorantButton.jsx
+++ src/app/buttons/cases/ValorantButton/ValorantButton.jsx
@@ -1,5 +1,11 @@
+import style from "./ValorantButton.module.css";
+
 function ValorantButton() {
-  return <h3>ValorantButton</h3>;
+  return (
+    <div className={style.buttonContainer}>
+      <p className={style.title}>PLAY</p>
+    </div>
+  );
 }
 
 export default ValorantButton;
