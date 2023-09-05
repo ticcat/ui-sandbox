58c6f56817a9296226a9f93ad3e86c30e98223a6
ticcat <serfgiorico@gmail.com>
2023-09-05 11:27:02 +0200

    Finished Valorant button styling

--- src/app/buttons/cases/ValorantButton/ValorantButton.jsx
+++ src/app/buttons/cases/ValorantButton/ValorantButton.jsx
@@ -4,6 +4,7 @@ function ValorantButton({ children }) {
   return (
     <div className={style.background}>
       <div className={style.buttonContainer}>
+        <div className={style.border}></div>
         <p className={style.title}>{children ? children : "PLAY"}</p>
       </div>
     </div>
