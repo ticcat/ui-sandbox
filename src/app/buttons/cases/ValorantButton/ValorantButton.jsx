5c8c270a7203dc7dbceac2669ed864cb7665fff7
ticcat <serfgiorico@gmail.com>
2023-09-05 14:14:31 +0200

    Valorant button text centered

--- src/app/buttons/cases/ValorantButton/ValorantButton.jsx
+++ src/app/buttons/cases/ValorantButton/ValorantButton.jsx
@@ -5,7 +5,9 @@ export default function ValorantButton({ children }) {
     <div className={style.background}>
       <div className={style.buttonContainer}>
         <div className={style.border}></div>
-        <p className={style.title}>{children ? children : "PLAY"}</p>
+        <div className={style.title}>
+          <p>{children ? children : "PLAY"}</p>
+        </div>
       </div>
     </div>
   );
