70d5dbe802245469890fa67c3f0012eff825b167
ticcat <serfgiorico@gmail.com>
2023-09-05 12:28:58 +0200

    Added children prop to NoContentScreen with text to display

--- src/components/NoContentScreen/NoContentScreen.jsx
+++ src/components/NoContentScreen/NoContentScreen.jsx
@@ -1,9 +1,9 @@
 import style from "./NoContentScreen.module.css";
 
-export default function NoContentScreen() {
+export default function NoContentScreen({ children }) {
   return (
     <div className={style.noCaseContainer}>
-      <p className={style.title}>No study cases yet</p>
+      <p className={style.title}>{children}</p>
       <p className={style.sadFace}>😔</p>
     </div>
   );
