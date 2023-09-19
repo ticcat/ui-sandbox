c56da827f159c64675c2bed04ea93fc52d478eac
ticcat <serfgiorico@gmail.com>
2023-09-19 16:07:50 +0200

    Returned icons to public folder

--- src/components/ShowableButton/ShowableButton.jsx
+++ src/components/ShowableButton/ShowableButton.jsx
@@ -17,7 +17,7 @@ export default function ShowableButton({ show, iconName, onClickHandler }) {
     >
       <Image
         alt={iconName + " button"}
-        src={"/src/icons/" + iconName + ".svg"}
+        src={"/icons/" + iconName + ".svg"}
         width={30}
         height={30}
       ></Image>
