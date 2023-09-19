77fc3b21bf9ca41c22ab5bf116084b2724f95cc6
ticcat <serfgiorico@gmail.com>
2023-09-19 16:35:02 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGitHubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePatch = "/";
+
+if (isGitHubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePatch = `/${repo}`;
+}
+
 const nextConfig = {
   output: "export",
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
+  assetPrefix: assetPrefix,
+  basePatch: basePatch,
 };
 
 module.exports = nextConfig;
