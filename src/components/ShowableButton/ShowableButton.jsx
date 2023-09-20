ac143133d71538f6e955f67e65eafc92e9547d94
ticcat <serfgiorico@gmail.com>
2023-09-20 15:22:53 +0200

    Deleted now unnecessary repo hook and refactored unnecessary dependencies on showable button

--- src/components/ShowableButton/ShowableButton.jsx
+++ src/components/ShowableButton/ShowableButton.jsx
@@ -1,15 +1,8 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import styles from "./ShowableButton.module.css";
 
 import Image from "next/image";
-import { useRepo } from "@/hooks/RepoHooks";
 
 export default function ShowableButton({ show, iconName, onClickHandler }) {
-  // TODO: Add appear condition to props and rename to HiddeblebeButton
-  const repoName = useRepo();
-
   return (
     <button
       style={!show ? { opacity: 1 } : { opacity: 0 }}
