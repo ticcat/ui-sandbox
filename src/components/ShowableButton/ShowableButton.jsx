f0f5a412568a6cfcee9b9584b88023831363091c
ticcat <serfgiorico@gmail.com>
2023-09-20 13:31:57 +0200

    Hardcoding repo name to button

--- src/components/ShowableButton/ShowableButton.jsx
+++ src/components/ShowableButton/ShowableButton.jsx
@@ -18,7 +18,7 @@ export default function ShowableButton({ show, iconName, onClickHandler }) {
     >
       <Image
         alt={iconName + " button"}
-        src={repoName + "/icons/" + iconName + ".svg"}
+        src={"ui-sanbox/icons/" + iconName + ".svg"}
         width={30}
         height={30}
       ></Image>
