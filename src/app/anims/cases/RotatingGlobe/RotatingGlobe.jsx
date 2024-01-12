796fab35d762083210b179943b61c1e800794b4d
ticcat <serfgiorico@gmail.com>
2024-01-12 14:07:57 +0100

    Finished rotating globe implementation

--- src/app/anims/cases/RotatingGlobe/RotatingGlobe.jsx
+++ src/app/anims/cases/RotatingGlobe/RotatingGlobe.jsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./RotatingGlobe.module.css";
+
 export default function RotatingGlobe() {
-  return <h1>Rotating globe</h1>;
+  return (
+    <div className={styles.globeWrapper}>
+      <div className={styles.globeExternal}>
+        <div className={styles.verLine1}></div>
+        <div className={styles.verLine2}></div>
+        <div className={styles.verLine3}></div>
+        <div className={styles.horLines}></div>
+        <div className={styles.middleLine}></div>
+      </div>
+    </div>
+  );
 }
