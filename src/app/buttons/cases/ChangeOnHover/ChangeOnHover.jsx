e99f94dd96e5b5ae226bb15e8ef028682640998b
ticcat <serfgiorico@gmail.com>
2024-01-17 11:20:45 +0100

    Added first gsap animation test

--- src/app/buttons/cases/ChangeOnHover/ChangeOnHover.jsx
+++ src/app/buttons/cases/ChangeOnHover/ChangeOnHover.jsx
@@ -1,3 +1,42 @@
+import styles from "./ChangeOnHover.module.css";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 export default function ChangeOnHoverButton() {
-  return <h1>Change on hover</h1>;
+  const text = "Click me!";
+
+  console.log(styles.letter);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      "#letter",
+      {
+        scale: 1,
+      },
+      {
+        scale: 2,
+        stagger: 0.2,
+        repeat: 1,
+        yoyo: true,
+        duration: 0.1,
+      }
+    );
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        {text.split("").map((letter, index) =>
+          letter === " " ? (
+            <span key={index}>&nbsp;</span>
+          ) : (
+            <span id="letter" key={index}>
+              {letter}
+            </span>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
