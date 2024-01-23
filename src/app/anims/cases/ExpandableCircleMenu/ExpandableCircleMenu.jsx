8bee3d053c292e6d8ad07378d7383fdbf1aa6619
ticcat <serfgiorico@gmail.com>
2024-01-23 13:34:24 +0100

    Added expandable circle menu implementation

--- src/app/anims/cases/ExpandableCircleMenu/ExpandableCircleMenu.jsx
+++ src/app/anims/cases/ExpandableCircleMenu/ExpandableCircleMenu.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import styles from "./ExpandableCircleMenu.module.css";
+
 export default function ExpandableCircleMenu() {
-  return <h1>Expandable circle menu</h1>;
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className={`${styles.container} ${hover && styles.active}`}>
+      <ul className={styles.buttonsList}>
+        <li className={`${styles.button} ${styles.text}`}>our products</li>
+        <li className={`${styles.button} ${styles.text}`}>culture n&apos;CO</li>
+        <li className={`${styles.button} ${styles.text}`}>Who we are</li>
+        <li className={`${styles.button} ${styles.text}`}>Join the family</li>
+      </ul>
+      <div
+        className={styles.circleBox}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      ></div>
+    </div>
+  );
 }
